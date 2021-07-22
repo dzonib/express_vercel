2c1204ead5e630c05c5d5f48b54c6126ac6ff762
bosnjaknikola <nikola.bosnjak@wayseven.com>
2021-07-22 08:19:56 +0200

    fixing route

--- api/product.js
+++ api/product.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/test', async (req, res) => {
+router.get('', async (req, res) => {
   try {
     res.json({
       status: 200,
